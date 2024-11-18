87d0bc4fc8ebb49dc875f1edc1134b055315a9e1
ghulammuzz <ghulamxiaomi@gmail.com>
2024-11-18 21:07:46 +0700

    cicd: trigger
    Signed-off-by: ghulammuzz <ghulamxiaomi@gmail.com>

--- internal/health/handler.go
+++ internal/health/handler.go
@@ -18,7 +18,7 @@ func HealthCheck(db *sql.DB) fiber.Handler {
 
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
 			"status":  "ok",
-			"message": "Service is healthy",
+			"message": "Service is healthyyyy",
 		})
 	}
 }
